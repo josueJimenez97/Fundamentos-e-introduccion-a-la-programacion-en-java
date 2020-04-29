9ab569eeea19eeb699a30b63ed6cd709bf56ea73
josueJimenez97 <josuejnumss97@gmail.com>
2020-04-29 15:30:23 -0400

    ejemplos de la unidad 5

---
+++ ejemploUnidad5/DiasSemana.java
@@ -0,0 +1,21 @@
+public class DiasSemana{
+    public String diaSemana(int n){
+        String res="ese dia no existe";
+        if(n==1){
+            res="lunes";
+        }else if(n==2){
+            res="martes";
+        }else if(n==3){
+            res="miercoles";   
+        }else if(n==4){
+            res="jueves";   
+        }else if(n==5){
+            res="viernes";
+        }else if(n==6){
+            res="sabado";
+        }else if(n==7){
+            res="domingo";
+        }
+        return res;
+    }
+}
